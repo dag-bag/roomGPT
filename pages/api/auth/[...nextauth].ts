95dacb4bc63c8ce8108c81b039bb0c47245ffe8a
dag-bag <88625252+dag-bag@users.noreply.github.com>
2023-05-02 02:52:25 -0700

    email auth init

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -2,6 +2,7 @@ import NextAuth, { NextAuthOptions } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
 import prisma from "../../../lib/prismadb";
+import EmailProvider from "next-auth/providers/email";
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
@@ -9,6 +10,10 @@ export const authOptions: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+    }),
+    EmailProvider({
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM,
     }),
   ],
 };
--- pages/dream.tsx
+++ pages/dream.tsx
@@ -16,7 +16,7 @@ import downloadPhoto from "../utils/downloadPhoto";
 import DropDown from "../components/DropDown";
 import { roomType, rooms, themeType, themes } from "../utils/dropdownTypes";
 import { GenerateResponseData } from "./api/generate";
-import { useSession, signIn } from "next-auth/react";
+import { useSession, signIn, signOut } from "next-auth/react";
 import useSWR from "swr";
 import { Rings } from "react-loader-spinner";
 import Link from "next/link";
@@ -292,6 +292,22 @@ const Home: NextPage = () => {
                         alt="google's logo"
                       />
                       <span>Sign in with Google</span>
+                    </button>
+                    <button
+                      onClick={() =>
+                        signIn("email", {
+                          email: "virenderkumar23435@gmail.com",
+                        })
+                      }
+                      className="bg-gray-200 text-black font-semibold py-3 px-6 rounded-2xl flex items-center space-x-2"
+                    >
+                      {/* <Image
+                        src="/google.png"
+                        width={20}
+                        height={20}
+                        alt="google's logo"
+                      /> */}
+                      <span>Sign in with Email</span>
                     </button>
                   </div>
                 )
